e83243bd95af9e8b2a2deabeb981738c0594693d
Yossi Farjoun <farjoun@broadinstitute.org>
2021-03-05 16:02:03 -0500

    and now for something completely different

--- installation/create_env.sh
+++ installation/create_env.sh
@@ -4,7 +4,7 @@
 ENV=vitaminD_test
 set +eu \
   && PS1=dummy \
-  && conda init
+  && conda init \
   && . $(conda info --base)/etc/profile.d/conda.sh \
   && conda activate base \
   && conda install -y -c conda-forge mamba 
