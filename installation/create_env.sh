67e5a1a498551638bfaa26150ec6bb055d788b4f
Yossi Farjoun <farjoun@broadinstitute.org>
2021-03-05 13:16:05 -0500

    This PR introduces github actions to
    - build a docker image that is ready to go
    - build the conda environment and test it by loading TwoSampleMR and LDlinkR libraries in R
    - run lintr against the R code to "force" a coding standard.

--- installation/create_env.sh
+++ installation/create_env.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-ENV=vitaminD_covid_MR2
+BASEDIR=$(dirname $0)
+
+ENV=VitaminD_MR
 set +eu \
-  && PS1=dummy \
+  && PS1='$$$ ' \
   && . $(conda info --base)/etc/profile.d/conda.sh \
-  && conda activate base;
+  && conda activate base \
+  && conda install -y -c conda-forge mamba 
 set -eu;
 
+conda deactivate || echo "no active environment"
+conda remove -n ${ENV} || echo "couldn't remove environment ${ENV}"
+conda create -y -n ${ENV}  || echo "it seem that environment ${ENV} is already present"
 
-mamba create --yes \
-	--name ${ENV} \
-	--channel bioconda \
-	--channel conda-forge \
-	--channel r \
-	--file requirements.txt
+set +eu
+mamba env update \
+	--file $BASEDIR/environment.yaml
+set -eu
 
-conda activate ${ENV}
-
-R < post_conda_steps.R
-
+echo CREATED the environment ${ENV}
---
+++ installation/post_conda_steps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## run post-conda steps
+BASEDIR=$(dirname $0)
+
+echo RUNNING post-conda steps
+
+set +e \
+  && PS1='$$$ ' \
+  && . $(conda info --base)/etc/profile.d/conda.sh \
+  && conda activate base 
+set -e
+
+conda activate VitaminD_MR
+
+R --no-save < $BASEDIR/post_conda_steps.R
+
