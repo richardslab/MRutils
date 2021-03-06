41213cd0b09c73dc8a8c1e1306ceb10c18209a8c
Yossi Farjoun <farjoun@broadinstitute.org>
2021-03-05 21:08:35 -0500

    adding openxlsx

--- installation/post_conda_steps.sh
+++ installation/post_conda_steps.sh
@@ -1,11 +1,6 @@
-#source me
-
-ENV=vitaminD_MR
-
-conda activate ${ENV}
 
 ## run post-conda steps
 echo RUNNING post-conda steps
-
-R --no-save < installation/post_conda_steps.R
+conda activate vitaminD_MR
+R --no-save < post_conda_steps.R
 
