b258bed797c0d388e29e791480c90eb66af2ceb7
Yossi Farjoun <farjoun@broadinstitute.org>
2021-03-11 14:43:51 -0500

    fixed cleanshell complaints

--- installation/post_conda_steps.sh
+++ installation/post_conda_steps.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 ## run post-conda steps
-BASEDIR=$(dirname $0)
+BASEDIR="$(dirname $0)"
 
 echo RUNNING post-conda steps
 
 set +e \
   && PS1='$$$ ' \
-  && . $(conda info --base)/etc/profile.d/conda.sh \
+  && . "$(conda info --base)"/etc/profile.d/conda.sh \
   && conda activate base 
 set -e
 
 conda activate VitaminD_MR
 
-R --no-save < $BASEDIR/post_conda_steps.R
+R --no-save < "$BASEDIR"/post_conda_steps.R
 
