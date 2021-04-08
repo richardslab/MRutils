aee1ea17c22097acedd4fdb7b5fa48ca0dcc3069
Yossi Farjoun <farjoun@broadinstitute.org>
2021-04-08 19:11:41 -0400

    renamed files, moved inline comments into block

--- installation/03-post_conda_steps.sh
+++ installation/03-post_conda_steps.sh
@@ -3,11 +3,8 @@
 ## run post-conda steps
 BASEDIR="$(dirname "$0")"
 
-# this PAT doesn't need any permissions.
-#GITHUB_PAT=$1 
 echo RUNNING post-conda steps
 
-# shellcheck source=/dev/null
 set +e \
   && PS1='$$$ ' \
   && . "$(conda info --base)"/etc/profile.d/conda.sh \
@@ -15,10 +12,6 @@ set +e \
 #set -e
 
 conda activate VitaminD_MR
-
-#if [[ grep -n PAT_GITHUB ~/.Renviron ]] ; then
-#	echo "GITHUB_PAT=${GITHUB_PAT}" >> ~/.Renviron
-#fi
 
 R --no-save < "$BASEDIR"/post_conda_steps.R
 
