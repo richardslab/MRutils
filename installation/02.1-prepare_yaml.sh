c9e94166c6bcc9a2164e6e69df11a8545b6372cd
Yossi Farjoun <farjoun@broadinstitute.org>
2021-04-15 16:19:21 -0400

    added a pre-conda step

---
+++ installation/02.1-prepare_yaml.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    *)          machine="UNKNOWN"
+esac
+
+echo ${machine}
+
+if [ $machine = "Mac" ]; then
+  sed '/{{linux-only}}/d' "$BASEDIR"/environment.yaml  > "$BASEDIR"/environment_modified.yaml 
+else
+  cp "$BASEDIR"/environment.yaml "$BASEDIR"/environment_modified.yaml 
+fi
