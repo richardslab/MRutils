80e191a1bb4dc6849c805f0d413a6f6ae915a471
Yossi Farjoun <farjoun@broadinstitute.org>
2021-04-15 16:19:21 -0400

    added a pre-conda step

--- installation/02.2-create_env.sh
+++ installation/02.2-create_env.sh
@@ -2,15 +2,6 @@
 
 BASEDIR="$(dirname "$0")"
 
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Linux*)     machine=Linux;;
-    Darwin*)    machine=Mac;;
-    *)          machine="UNKNOWN"
-esac
-
-echo ${machine}
-
 ENV=MRutils
 # shellcheck source=/dev/null
 set +eu \
@@ -24,15 +15,8 @@ conda env remove -n ${ENV} || echo "Couldn't remove environment ${ENV}"
 conda create -y -n ${ENV}  || echo "It seem that environment ${ENV} is already present"
 
 set -e
-if [ $machine = "Mac" ]; then
-	sed '/{{linux-only}}/d' "$BASEDIR"/environment.yaml  > "$BASEDIR"/environment_modified.yaml 
-else
-	cp "$BASEDIR"/environment.yaml "$BASEDIR"/environment_modified.yaml 
-fi
-#set +e
-#set +eu
+
 mamba env update -q \
 	--file "$BASEDIR"/environment_modified.yaml 
-#set -eu
 
 echo CREATED the environment ${ENV}
